bee08d13f2c06d02bfec2b84c6b492f899169ba0
alamincoders <alaminsbl502@gmail.com>
2022-08-09 18:17:34 +0600

    intialize app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main className="App bg-red-300 text-black text-3xl">
+      <nav>
+        <h1> Welcome To Home</h1>
+      </nav>
+    </main>
+  );
+}
+
+export default App;
